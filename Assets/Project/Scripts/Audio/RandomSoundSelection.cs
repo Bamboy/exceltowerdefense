e5b6d4a0b36136921e26e7f421b22271f4fdee0b
Stephan Ennen <bamboy360@gmail.com>
2015-03-27 10:06:45 -0700

    changed import settings, added random sound player

---
+++ Assets/Project/Scripts/Audio/RandomSoundSelection.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils.Audio;
+
+public class RandomSoundSelection : MonoBehaviour 
+{
+	public AudioClip[] sounds;
+
+	void Awake()
+	{
+		AudioHelper.MasterVolume = 1.0f;
+		AudioHelper.EffectVolume = 1.0f;
+		AudioHelper.MusicVolume = 0.6f;
+		AudioHelper.VoiceVolume = 1.0f;
+	}
+
+	void Update () 
+	{
+		if( Input.GetKeyDown(KeyCode.Space) )
+		{
+			PlaySound();
+		}
+	}
+
+	void PlaySound()
+	{
+		sounds = ArrayTools.Shuffle< AudioClip >( sounds );
+		AudioHelper.PlayClipAtPoint( sounds[0], Vector3.zero, 0.75f, SoundType.Effect );
+	}
+}
