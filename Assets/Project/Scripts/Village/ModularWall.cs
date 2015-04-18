62e5c1b3d3f7004c39ef0a9c002be05c1238abe6
Stephan Ennen <bamboy360@gmail.com>
2015-04-18 16:55:49 -0700

    Further wall-ish stuff.

--- Assets/Project/Scripts/Village/ModularWall.cs
+++ Assets/Project/Scripts/Village/ModularWall.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//[ExecuteInEditMode]
+//Stephan Ennen 4/18/2015
+
+
 public class ModularWall : MonoBehaviour 
 {
 	public GameObject[] wallspikes; //Our prefabs. We will need to get the width
+	public GameObject wallObstacle; //Navmesh obstacle object
 
 	//Set of points that act as corners. The last entry in the array will draw to the first entry.
 	public Vector3[] wallpoints = new Vector3[2]; 
-	//TODO - we will want to do a raycast downward so that our walls are created on top of any hills.
+	//TODO - we will want to do a raycast downward so that our wall spikes are created on top of any hills.
 	
 
 	void OnDrawGizmos()
@@ -32,11 +35,12 @@ public class ModularWall : MonoBehaviour
 			Gizmos.DrawWireCube( wallpoints[j], Vector3.one );
 		}
 
+		/*
 		Gizmos.color = Color.black;
 		for(int k = 0; k < spikePoints.Length; k++)
 		{
 			Gizmos.DrawWireSphere( spikePoints[k], baseRadius / 2.0f );
-		}
+		} */
 
 
 	}
@@ -45,16 +49,18 @@ public class ModularWall : MonoBehaviour
 	[Range(0.1f, 10.0f)] public float baseRadius = 0.25f;
 	public int totalSpawnCount;
 	private int spawnCount;
-	private float extraDistance;
 
-	private float extraRadius;
 	void Start () 
 	{
 		CalculateSpikes();
 		SpawnSpikes();
 	}
-	
 
+	/*TODO
+		create a way to make gates or at least some way for enemies to get through the wall occasionally maybe
+		do this by testing to see if two points are in a certian distance range of each other and then only build
+		part of the wall for those two points.
+	*/
 	void CalculateSpikes()
 	{
 		totalSpawnCount = 0;
@@ -62,46 +68,53 @@ public class ModularWall : MonoBehaviour
 		if( wallspikes.Length > 0 && wallspikes[0] != null )
 		{
 			//Calculate spawn points for our spikes!
-			for(int line = 0; line < wallpoints.Length; line++)
+			for(int fromPoint = 0; fromPoint < wallpoints.Length; fromPoint++)
 			{
-				#region numtospawn
 				int toPoint;
-				if( line == wallpoints.Length - 1 )
+				if( fromPoint == wallpoints.Length - 1 )
 					toPoint = 0;
 				else
-					toPoint = line + 1;
-				
+					toPoint = fromPoint + 1;
+
+				#region NavmeshObstacle
+				//float realDist = Vector3.Distance( wallpoints[fromPoint], wallpoints[toPoint] );
+				GameObject navOb = (GameObject)Instantiate(wallObstacle, Vector3.Lerp(wallpoints[fromPoint], wallpoints[toPoint], 0.5f), 
+									Quaternion.identity);
+				//Quaternion.LookRotation(VectorExtras.Direction(wallpoints[fromPoint], wallpoints[toPoint]), Vector3.up));
+				navOb.transform.LookAt( wallpoints[toPoint] );
+				navOb.transform.localScale = new Vector3(1f, 1f, Vector3.Distance( wallpoints[fromPoint], wallpoints[toPoint] )); //This is the "real" distance
+				//navOb.transform.parent = this.transform;
+				#endregion
+
+
 				//We aren't interested in height during this calculation.
 				float flatDist = Vector3.Distance(
-					new Vector3( wallpoints[line].x, 0f, wallpoints[line].z ),
+					new Vector3( wallpoints[fromPoint].x, 0f, wallpoints[fromPoint].z ),
 					new Vector3( wallpoints[toPoint].x, 0f, wallpoints[toPoint].z ) );
 				
 				if( flatDist < baseRadius )
-					return;
+					continue;
 				
 				spawnCount = Mathf.FloorToInt( flatDist / baseRadius );
 				totalSpawnCount += spawnCount;
-				Debug.Log("Spawncount: "+ spawnCount);
+				//Debug.Log("Spawncount: "+ spawnCount);
 				
-				extraDistance = flatDist % baseRadius;
-				extraRadius = extraDistance / spawnCount;
+
 				
-				#endregion
-				
-				
+				#region Spike positioning
 				float progressPerSpike = 1.0f / spawnCount;
 				for(int spike = 0; spike < spawnCount; spike++)
 				{
 					Vector3 spawnPos = Vector3.Lerp(
-						new Vector3( wallpoints[line].x, 0f, wallpoints[line].z ),
+						new Vector3( wallpoints[fromPoint].x, 0f, wallpoints[fromPoint].z ),
 						new Vector3( wallpoints[toPoint].x, 0f, wallpoints[toPoint].z ),
 						(float)spike * progressPerSpike );
 					
 					spikePoints = ArrayTools.Push<Vector3>( spikePoints, spawnPos );
 				}
-				
+				#endregion
 			}
-			Debug.Log("Total created: "+ totalSpawnCount);
+			Debug.Log("Total wall spikes created: "+ totalSpawnCount);
 		}
 	}
 
@@ -124,10 +137,15 @@ public class ModularWall : MonoBehaviour
 
 
 
+	/* NAVMESHING:
+	 * 
+	 * Create object with NavMeshObstacle
+	 * Move object to be in the middle of the two points
+	 * make object rotate to face toward the second (target) point.
+	 * scale object along z axis. the amount to do this by is equal to distance between the two points.
 
 
-
-
+*/
 
 
 
---
+++ Assets/Project/Scripts/Village/ModularWallEditor.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode] [RequireComponent(typeof(ModularWall))]
+public class ModularWallEditor : MonoBehaviour 
+{
+	ModularWall walls;
+
+	public Transform[] movers;
+
+	// Use this for initialization
+	void Start () 
+	{
+#if UNITY_EDITOR
+		walls = GetComponent<ModularWall>();
+		Sync();
+		
+#else
+		Destroy( this );
+#endif
+	}
+
+	void Sync()
+	{
+		if( walls.wallpoints == null || walls.wallpoints.Length < 2 )
+		{
+			walls.wallpoints = new Vector3[2];
+			walls.wallpoints[0] = Vector3.zero; walls.wallpoints[1] = Vector3.one;
+		}
+		
+		movers = new Transform[0];
+		int id = 0;
+		foreach( Vector3 point in walls.wallpoints )
+		{
+			GameObject newMover = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			newMover.transform.position = point;
+			newMover.transform.parent = this.transform;
+			newMover.name = "Wall Corner "+ id;
+			movers = ArrayTools.InsertAt<Transform>( movers, newMover.transform, id );
+			id++;
+		}
+
+	}
+
+	void Update ()
+	{
+		if( walls.wallpoints.Length != movers.Length )
+		{
+			for(int i = 0; i < movers.Length; i++) //Remove old gameobjects
+			{
+				DestroyImmediate( movers[i].gameObject );
+			}
+			Sync();
+		}
+		else
+		{
+			for(int i = 0; i < movers.Length; i++)
+			{
+				walls.wallpoints[i] = movers[i].position;
+			}
+		}
+	}
+	void OnDestroy()
+	{
+		if( this.enabled == true )
+			Debug.LogError("Warning: Delete excess children in the gameobject or disable the attached editor script if you aren't using it!", this);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
